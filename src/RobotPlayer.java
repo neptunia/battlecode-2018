410a3d3a1682cb18a25ee6f44fb5c61a166c9523
VoidMercy <chingyuan.alex@gmail.com>
2018-01-09 18:32:48 -0500

    add while true

--- src/RobotPlayer.java
+++ src/RobotPlayer.java
@@ -7,46 +7,45 @@ public class RobotPlayer {
 	
 	public static void main(String args[]) {
 
-		GameController gc = new GameController();
+        GameController gc = new GameController();
 
-        long currentRound = gc.round();
+                
+        while (true) {
+            long currentRound = gc.round();
+            VecUnit myUnits = gc.myUnits();
+            long numberOfUnits = myUnits.size();
 
-        MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
+                    
 
-        VecUnit myUnits = gc.myUnits();
-        long numberOfUnits = myUnits.size();
+            //iterate through units
+            // might need to fix this later; what happens if I create a new unit in the middle of this loop?
+            for (int i = 0; i < numberOfUnits; i++) {
+                Unit curUnit = myUnits.get(i);
+                //perform unit task based on unit type
+                switch (curUnit.unitType()) {
+                    case UnitType.Factory:
+                    	Factory.run(gc, curUnit);
+                    case UnitType.Healer:
+                    	Healer.run(gc, curUnit);
+                    case UnitType.Knight:
+                    	Knight.run(gc, curUnit);
+                    case UnitType.Mage:
+                    	Mage.run(gc, curUnit);
+                    case UnitType.Ranger:
+                    	Ranger.run(gc, curUnit);
+                    case UnitType.Rocket:
+                    	Rocket.run(gc, curUnit);
+                    case UnitType.Worker:
+                    	Worker.run(gc, curUnit);
+                }
+            }
 
-        
+            //do research
+            gc.queueResearch(UnitType.Ranger);
+            gc.queueResearch(UnitType.Rocket);
 
-        //iterate through units
-        // might need to fix this later; what happens if I create a new unit in the middle of this loop?
-        for (int i = 0; i < numberOfUnits; i++) {
-        	Unit curUnit = myUnits.get(i);
-        	//perform unit task based on unit type
-        	switch (curUnit.unitType()) {
-        		case UnitType.Factory:
-        			Factory.run(gc, curUnit);
-        		case UnitType.Healer:
-        			Healer.run(gc, curUnit);
-        		case UnitType.Knight:
-        			Knight.run(gc, curUnit);
-        		case UnitType.Mage:
-        			Mage.run(gc, curUnit);
-        		case UnitType.Ranger:
-        			Ranger.run(gc, curUnit);
-        		case UnitType.Rocket:
-        			Rocket.run(gc, curUnit);
-        		case UnitType.Worker:
-        			Worker.run(gc, curUnit);
-        	}
+            //end turn
+            gc.nextTurn();
         }
-
-        //do research
-        gc.queueResearch(UnitType.Ranger);
-        gc.queueResearch(UnitType.Rocket);
-
-        //end turn
-        gc.nextTurn();
 	}
-
 }
