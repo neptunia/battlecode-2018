80c9c1052de17b1f49e03676574d5de3a2d691e4
VoidMercy <chingyuan.alex@gmail.com>
2018-01-09 18:25:11 -0500

    knight code edit

--- src/Knight.java
+++ src/Knight.java
@@ -13,13 +13,38 @@ public class Knight {
 		MapLocation location = unit.location().mapLocation();
 
 		//attack enemies that are near you
-		//TODO change team number!!
+		if (canAttack()) {
+			attackNearbyEnemies();
+		}
+
+		if (canMove()) {
+			move();
+		}
+	}
+
+	public static boolean canAttack() {
+		return curUnit.attackHeat() < 10:
+	}
+
+	public static void attackNearbyEnemies() {
 		VecUnit nearbyUnits = gc.senseNearbyUnits(location, curUnit.attackRange());
 		for (int i = 0; i < nearbyUnits.size(); i++) {
 			Unit unit = nearbyUnits.get(i);
 			//if can attack this enemy unit
 			if (unit.team != gc.team() && gc.isAttackReady(curUnit.id()) && gc.canAttack(curUnit.id(), unit.id())) {
 				gc.attack(unit.id(), curUnit.id());
+			}
+		}
+	}
+
+	public static boolean canMove() {
+		return curUnit.movementHeat() < 10;
+	}
+
+	public static void move() {
+		for (int i = 0; i < directions.length; i++) {
+			if (gc.isMoveReady(curUnit.id()) && gc.canMove(curUnit.id(), directions[i])) {
+				gc.moveRobot(curUnit.id(), directions[i]);
 			}
 		}
 	}
