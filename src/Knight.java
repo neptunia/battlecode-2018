fcac16d1dc74632783e6f860affb6fde1107ec43
VoidMercy <chingyuan.alex@gmail.com>
2018-01-10 21:03:46 -0500

    knight pathfinding

--- src/Knight.java
+++ src/Knight.java
@@ -5,6 +5,7 @@ public class Knight {
 	static Unit curUnit;
 	static GameController gc;
 	static Direction[] directions = Direction.values();
+	static HashMap<Integer, String> visited = new HashMap<Integer, String>();
 
 	public static void run(GameController gc, Unit curUnit) {
 
@@ -21,7 +22,7 @@ public class Knight {
 		}
 
 		if (canMove()) {
-			move();
+			move(getTarget());
 		}
 	}
 
@@ -46,15 +47,31 @@ public class Knight {
 	}
 
 	//pathing
-	public static void move() {
-		MapLocation target = getTarget();
-		//TODO implement nav to target
+	//move towards target location
+	public static boolean move(MapLocation target) {
+		//TODO implement pathfinding
+		double smallest = 999999;
+		Direction d = null;
+		MapLocation curLoc = curUnit.location().mapLocation();
+		int hash = hash(curLoc.getX(), curLoc.getY());
+		if (!visited.containsKey(curUnit.id())) {
+			visited.put(curUnit.id(), Integer.toString(hash) + ",");
+		} else {
+			visited.put(curUnit.id(), visited.get(curUnit.id()) + Integer.toString(hash) + ",");
+		}
 		for (int i = 0; i < directions.length; i++) {
-			if (gc.isMoveReady(curUnit.id()) && gc.canMove(curUnit.id(), directions[i])) {
-				gc.moveRobot(curUnit.id(), directions[i]);
-				return;
+			MapLocation newSquare = curLoc.add(directions[i]);
+			if (!visited.get(curUnit.id()).contains(Integer.toString(hash(newSquare.getX(), newSquare.getY()))) && gc.canMove(curUnit.id(), directions[i]) && distance(newSquare, target) < smallest) {
+				smallest = distance(newSquare, target);
+				d = directions[i];
 			}
 		}
+		if (d == null) {
+			//can't move
+			return false;
+		}
+		gc.moveRobot(curUnit.id(), d);
+		return true;
 	}
 
 	//get target unit should be pathing towards
