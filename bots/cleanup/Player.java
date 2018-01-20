06205976d17b52af60b310adaf3ee3eea28c8f03
VoidMercy <chingyuan.alex@gmail.com>
2018-01-19 20:55:59 -0500

    try catches

--- bots/cleanup/Player.java
+++ bots/cleanup/Player.java
@@ -91,7 +91,7 @@ public class Player {
                 if (gc.planet() == Planet.Earth && gc.round() >= 750) {
                     gc.nextTurn();
                 }
-
+                
                 currentIncome = 10 - Math.max(gc.karbonite() / 40, 0);
                 long currentRound = gc.round();
                 VecUnit myUnits = gc.myUnits();
@@ -141,40 +141,49 @@ public class Player {
                                 Worker.run(curUnit);
                         }
                     } catch (Exception e) {
-                        //System.out.println("unit died");
+                        System.out.println("unit died");
                         e.printStackTrace();
                     }
                 }
+                try {
+                    chooseTarget();
+                } catch (Exception e) {
+                    System.out.println("choose target borked");
+                    e.printStackTrace();
+                }
+                
 
-                chooseTarget();
 
+                try {
+                    endTime = gc.getTimeLeftMs();
+                    total += startTime - endTime;
+                    averageTime = total / gc.round();
+                    //System.out.println("Time: " + Long.toString(startTime - endTime));
+                    //System.out.println("Average: " + Float.toString(averageTime));
 
+                    //System.out.println("Time Left: " + Long.toString(gc.getTimeLeftMs()));
+                    prevIncome = currentIncome;
+                    //Runtime runtime = Runtime.getRuntime();
 
-                endTime = gc.getTimeLeftMs();
-                total += startTime - endTime;
-                averageTime = total / gc.round();
-                //System.out.println("Time: " + Long.toString(startTime - endTime));
-                //System.out.println("Average: " + Float.toString(averageTime));
+                    //NumberFormat format = NumberFormat.getInstance();
 
-                //System.out.println("Time Left: " + Long.toString(gc.getTimeLeftMs()));
-                prevIncome = currentIncome;
-                //Runtime runtime = Runtime.getRuntime();
+                    //StringBuilder sb = new StringBuilder();
+                    //long maxMemory = runtime.maxMemory();
+                    //long allocatedMemory = runtime.totalMemory();
+                    //long freeMemory = runtime.freeMemory();
 
-                //NumberFormat format = NumberFormat.getInstance();
-
-                //StringBuilder sb = new StringBuilder();
-                //long maxMemory = runtime.maxMemory();
-                //long allocatedMemory = runtime.totalMemory();
-                //long freeMemory = runtime.freeMemory();
-
-                //sb.append("free memory: " + format.format(freeMemory / 1024) + "<br/>");
-                //sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
-                //sb.append("max memory: " + format.format(maxMemory / 1024) + "<br/>");
-                //sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");
-                //System.out.println(sb);
-                //System.out.println("Enemy location: " + Integer.toString(enemyLocation.getX()) + ", " + Integer.toString(enemyLocation.getY()));
-                //System.out.println("Round: " + Long.toString(gc.round()));
-                startTime = endTime;
+                    //sb.append("free memory: " + format.format(freeMemory / 1024) + "<br/>");
+                    //sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "<br/>");
+                    //sb.append("max memory: " + format.format(maxMemory / 1024) + "<br/>");
+                    //sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "<br/>");
+                    //System.out.println(sb);
+                    //System.out.println("Enemy location: " + Integer.toString(enemyLocation.getX()) + ", " + Integer.toString(enemyLocation.getY()));
+                    //System.out.println("Round: " + Long.toString(gc.round()));
+                    startTime = endTime;
+                } catch (Exception e) {
+                    System.out.println("time borked");
+                    e.printStackTrace();
+                }
                 gc.nextTurn();
             }
         } catch (Exception e) {
