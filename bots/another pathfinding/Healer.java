c17100ffb8c372a53dc458c078caf40aa7395f1e
ClaudeZou <claude.zou.work@gmail.com>
2018-01-20 11:11:27 -0500

    Fix bug

--- bots/another pathfinding/Healer.java
+++ bots/another pathfinding/Healer.java
@@ -45,15 +45,15 @@ public class Healer {
         int closestEnemy = enemyTooClose();
         if (closestEnemy != -1) {
             // there is an enemy within 70 units of me. This is bad, move away.
-            if (canHeal()) {
-                healNearbyAllies();
-            }
             if (gc.isMoveReady(curUnit.id())) {
                 moveAway(gc.unit(closestEnemy).location().mapLocation());
             }
 
-            // then heal whoever's available 
-            
+            // then heal whoever's available
+            if (canHeal()) {
+                healNearbyAllies();
+            }
+
             return;
         }
 
@@ -182,7 +182,9 @@ public class Healer {
                 }
             }
         }
-        gc.heal(curUnit.id(), id);
+        if (id != -1) {
+            gc.heal(curUnit.id(), id);
+        }
     }
 
     public static void healWeakestAllies() {
