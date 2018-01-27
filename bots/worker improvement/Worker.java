91aada6d6edc8f4e59a429ec18a337a59f05e1cd
VoidMercy <chingyuan.alex@gmail.com>
2018-01-27 14:34:14 -0500

    stop making factories after 600, go to mars

--- bots/worker improvement/Worker.java
+++ bots/worker improvement/Worker.java
@@ -77,7 +77,7 @@ public class Worker {
         System.out.println("---------------------");
         System.out.println(structuresToBuild.size());
         System.out.println(structures.size());*/
-        if (Player.prevBlocked < 10 && gc.karbonite() + Player.karboniteGonnaUse >= 200 && gc.round() != 1 && (Player.numFactory + structuresToBuild.size() < (split.get(myId) ? 3 : 6)) || gc.karbonite() + Player.karboniteGonnaUse >= 1000) {
+        if (Player.prevBlocked < 10 && gc.karbonite() + Player.karboniteGonnaUse >= 200 && gc.round() != 1 && gc.round() < 600 && (Player.numFactory + structuresToBuild.size() < (split.get(myId) ? 3 : 6)) || gc.karbonite() + Player.karboniteGonnaUse >= 1000) {
             startStructure(UnitType.Factory);
             Worker.run(curUnit);
         } else if (gc.karbonite() + Player.karboniteGonnaUse >= 150 && Player.numFactory >= 1 && gc.researchInfo().getLevel(UnitType.Rocket) > 0 && Player.numRanger - numRangerGoingToRocket >= 5 && Player.numHealer - numHealerGoingToRocket >= 2) {
