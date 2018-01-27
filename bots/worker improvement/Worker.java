ea931d2298fc265c7878b8c23d9197b828c2fe3e
VoidMercy <chingyuan.alex@gmail.com>
2018-01-27 12:19:15 -0500

    changes

--- bots/worker improvement/Worker.java
+++ bots/worker improvement/Worker.java
@@ -63,7 +63,7 @@ public class Worker {
             return;
         }
         ///&& Player.numFactory + structuresToBuild.size() < 4 
-        if (Player.prevBlocked < 10 && Player.timesReachedTarget < 1 && gc.karbonite() + Player.karboniteGonnaUse >= 200 && gc.round() != 1 && Player.numFactory + structuresToBuild.size() < (split.get(myId) ? 3 : 6)) {
+        if (Player.prevBlocked < 10 && gc.karbonite() + Player.karboniteGonnaUse >= 200 && gc.round() != 1 && (Player.numFactory + structuresToBuild.size() < (split.get(myId) ? 3 : 6)) || gc.karbonite() + Player.karboniteGonnaUse >= 1000) {
             startStructure(UnitType.Factory);
             Worker.run(curUnit);
         } else if (gc.karbonite() + Player.karboniteGonnaUse >= 150 && Player.numFactory >= 1 && gc.researchInfo().getLevel(UnitType.Rocket) > 0 && Player.numRanger - numRangerGoingToRocket >= 5 && Player.numHealer - numHealerGoingToRocket >= 2) {
