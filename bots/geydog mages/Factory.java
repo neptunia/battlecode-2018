e5e52bdb87e9555f43ca9a1c46b44c2a14e4041c
neptunia <kevinshen100@gmail.com>
2018-01-30 22:24:23 -0500

    test mage shits

---
+++ bots/geydog mages/Factory.java
@@ -0,0 +1,130 @@
+import bc.*;
+import java.util.*;
+
+public class Factory {
+
+	static Unit curUnit;
+	static MapLocation curLoc;
+	static GameController gc;
+	static int myId;
+	static HashSet<Integer> workers = new HashSet<Integer>();
+	static Direction[] directions = Direction.values();
+	static HashSet<Integer> sentHealSignal = new HashSet<Integer>();
+	static HashMap<Integer, Integer> thingsProduced = new HashMap<Integer, Integer>();
+
+	public static void run(Unit curUnit) {
+
+		Factory.curUnit = curUnit;
+		Factory.curLoc = curUnit.location().mapLocation();
+		if (!thingsProduced.containsKey(curUnit.id())) {
+			thingsProduced.put(curUnit.id(), 0);
+		}
+
+		if (curUnit.structureIsBuilt() == 0) {
+			return;
+		}
+		myId = Worker.id.get(curUnit.id());
+
+		if (curUnit.health() != curUnit.maxHealth()) {
+			findWorkersToHealMe();
+			sentHealSignal.add(curUnit.id());
+		} else if (sentHealSignal.contains(curUnit.id())) {
+			sentHealSignal.remove(curUnit.id());
+		}
+
+		VecUnitID garrison = curUnit.structureGarrison();
+		for (int i = 0; i < garrison.size(); i++) {
+			//unload units
+			for (int a = 0; a < directions.length; a++) {
+				if (gc.canUnload(curUnit.id(), directions[a])) {
+					gc.unload(curUnit.id(), directions[a]);
+					Unit newUnit = gc.senseUnitAtLocation(curLoc.add(directions[a]));
+					int newId = newUnit.id();
+					Worker.id.put(newId, myId);
+					Player.newUnits.add(newUnit);
+					break;
+				}
+			}
+		}
+
+		if (Player.numWorker < 1 && gc.canProduceRobot(curUnit.id(), UnitType.Worker)) {
+			gc.produceRobot(curUnit.id(), UnitType.Worker);
+			Player.numWorker++;
+		}
+
+		if ((Player.prevBlocked < 8 && Player.timesReachedTarget < 1 && gc.round() < (Worker.split.get(myId) ? 75 : 570) && (gc.karbonite() + Player.karboniteGonnaUse > 140 || gc.researchInfo().getLevel(UnitType.Rocket) == 0)) || (Player.numRanger < 8 || Player.numHealer < 5)) {
+			if ((Player.numRanger + Player.numMage) * 4 > 7 * Player.numHealer && gc.researchInfo().getLevel(UnitType.Healer) >= 1 && gc.canProduceRobot(curUnit.id(), UnitType.Healer)) {
+				gc.produceRobot(curUnit.id(), UnitType.Healer);
+				Player.numHealer++;
+			} else if (gc.canProduceRobot(curUnit.id(), UnitType.Healer) && gc.round() < 200 && gc.senseNearbyUnitsByTeam(curLoc, 50, Player.enemyTeam).size() != 0) {
+				if (thingsProduced.get(curUnit.id()) % 2 == 0) {
+					gc.produceRobot(curUnit.id(), UnitType.Knight);
+					Player.numKnight++;
+				} else {
+					gc.produceRobot(curUnit.id(), UnitType.Mage);
+				}
+				thingsProduced.put(curUnit.id(), thingsProduced.get(curUnit.id())+1);
+			} else if ((Player.numRanger + Player.numHealer) > 10 * Player.numMage && gc.canProduceRobot(curUnit.id(), UnitType.Mage)) {
+				gc.produceRobot(curUnit.id(), UnitType.Mage);
+				Player.numMage++;
+			} else if (gc.canProduceRobot(curUnit.id(), UnitType.Ranger)) {
+				gc.produceRobot(curUnit.id(), UnitType.Ranger);
+				Player.numRanger++;
+			}
+		}
+
+	}
+
+	public static void findWorkersToHealMe() {
+		LinkedList<MapLocation> queue = new LinkedList<MapLocation>();
+		HashSet<Integer> visited = new HashSet<Integer>();
+		queue.add(curLoc);
+		visited.add(hash(curLoc));
+		int workerFound = 0;
+		while (!queue.isEmpty()) {
+			MapLocation current = queue.poll();
+			if (manDistance(current, curLoc) > 2 && workerFound >= 2) {
+				return;
+			}
+			if (gc.hasUnitAtLocation(current)) {
+				Unit theUnit = gc.senseUnitAtLocation(current);
+				//if its' a worker on my team and he's not building/healing anything
+				if (theUnit.team() == Player.myTeam && theUnit.unitType() == UnitType.Worker && !Worker.structures.containsKey(theUnit.id()) && !Worker.healFactory.containsKey(theUnit.id())) {
+					workerFound++;
+					Worker.healFactory.put(theUnit.id(), curLoc);
+				}
+			}
+			for (int i = 0; i < directions.length; i++) {
+				MapLocation test = current.add(directions[i]);
+				if (!visited.contains(hash(test)) && checkPassable(test)) {
+					queue.add(test);
+					visited.add(hash(test));
+				}
+			}
+		}
+	}
+
+	public static int manDistance(MapLocation first, MapLocation second) {
+        int x1 = first.getX(), y1 = first.getY(), x2 = second.getX(), y2 = second.getY();
+        return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2));
+    }
+
+	public static boolean checkPassable(MapLocation test) {
+        int x = test.getX();
+        int y = test.getY();
+        if (x >= Player.gridX || y >= Player.gridY || x < 0 || y < 0) {
+            return false;
+        }
+        return Player.gotoable[myId][x][y];// && !allyThere;
+    }
+
+	public static int hash(MapLocation loc) {
+		return 69 * loc.getX() + loc.getY();
+	}
+
+	public static boolean onMap(MapLocation test) {
+        int x = test.getX();
+        int y = test.getY();
+        return x >= 0 && y >= 0 && x < Player.gridX && y < Player.gridY;
+    }
+}
---
+++ bots/geydog mages/Rocket.java
@@ -0,0 +1,157 @@
+import bc.*;
+import java.util.*;
+
+public class Rocket {
+
+    static Unit curUnit;
+    static MapLocation curLoc;
+    static GameController gc;
+    static Direction[] directions = Direction.values();
+    static int myId;
+    static HashSet<Integer> workersPerRocket = new HashSet<Integer>();
+    static HashMap<Integer, Integer> turnsSinceBuilt = new HashMap<Integer, Integer>();
+    //static HashMap<Integer, HashSet<Integer>> assignedUnits = new HashMap<Integer, HashSet<Integer>>();
+    static HashSet<Integer> assignedUnits = new HashSet<Integer>();
+    static int landSpotNumber = 0;
+    static boolean initialized = false;
+
+    public static void run(Unit curUnit) {
+
+        Rocket.curUnit = curUnit;
+
+
+        if (curUnit.location().isInSpace()) {
+            //in space
+            return;
+        }
+
+        Rocket.curLoc = curUnit.location().mapLocation();
+
+        if (curUnit.location().isOnPlanet(Planet.Earth)) {
+            if (curUnit.health() == curUnit.maxHealth()) {
+                if (gc.round() == 749) {
+                    launch();
+                }
+                if (!turnsSinceBuilt.containsKey(curUnit.id())) {
+                    turnsSinceBuilt.put(curUnit.id(), 1);
+                } else if (turnsSinceBuilt.get(curUnit.id()) > 100 && curUnit.structureGarrison().size() != 0) {
+                    launch();
+                } else {
+                    turnsSinceBuilt.put(curUnit.id(), turnsSinceBuilt.get(curUnit.id())+1);
+                }
+            } else if (curUnit.structureGarrison().size() != 0) {
+                launch();
+            }
+            //on earth, load units
+            VecUnit adjacentUnits = gc.senseNearbyUnitsByTeam(curUnit.location().mapLocation(), 2, Player.myTeam);
+            for (int i = 0; i < adjacentUnits.size(); i++) {
+                Unit unit = adjacentUnits.get(i);
+                int id = unit.id();
+                if (gc.canLoad(curUnit.id(), id) && Player.priorityTarget.containsKey(id)) {
+                    Player.priorityTarget.remove(id);
+                    gc.load(curUnit.id(), id);
+                    if (unit.unitType() == UnitType.Ranger) {
+                        Worker.numRangerGoingToRocket--;
+                    } else if (unit.unitType() == UnitType.Healer) {
+                        Worker.numHealerGoingToRocket--;
+                    }
+                }
+            }
+
+            if (curUnit.structureMaxCapacity() == curUnit.structureGarrison().size()) {
+                launch();
+            }
+        } else {
+            //initialize
+            if (!initialized) {
+                Player.marsInitialize();
+                initialized = true;
+            }
+            //assign a mars id
+            if (!Worker.id.containsKey(curUnit.id())) {
+                for (int i = 0; i < Player.gotoable.length; i++) {
+                    if (Player.gotoable[i][curLoc.getX()][curLoc.getY()]) {
+                        Worker.id.put(curUnit.id(), i);
+                        break;
+                    }
+                }
+            }
+            
+            myId = Worker.id.get(curUnit.id());
+            //on mars, unload units
+            MapLocation curLoc = curUnit.location().mapLocation();
+            for (int i = 0; i < directions.length; i++) {
+                if (gc.canUnload(curUnit.id(), directions[i])) {
+                    gc.unload(curUnit.id(), directions[i]);
+                    int newId = gc.senseUnitAtLocation(curUnit.location().mapLocation().add(directions[i])).id();
+                    Worker.id.put(newId, myId);
+                }
+            }
+        }
+
+    }
+
+    public static int hash(MapLocation loc) {
+        return 69 * loc.getX() + loc.getY();
+    }
+
+    public static void launch() {
+        //Player.loadingRocket = false;
+        //Player.enemyLocation = Player.pastLoc;
+
+        int hashLoc = gc.getTeamArray(Planet.Mars).get(landSpotNumber);
+        int y = hashLoc % 69;
+        int x = (hashLoc - y) / 69;
+        //System.out.println("X coordinate: " + Integer.toString(x));
+        //System.out.println("Y coordinate: " + Integer.toString(y));
+
+        //launch
+        MapLocation marsStart = new MapLocation(Planet.Mars, x, y);
+        if (gc.canLaunchRocket(curUnit.id(), marsStart)) {
+            HashSet<Integer> cantGo = new HashSet<Integer>();
+            MapLocation current = curUnit.location().mapLocation();
+            for (int i = 0; i < directions.length; i++) {
+                cantGo.add(hash(current.add(directions[i])));
+            }
+            for (int i = 0; i < directions.length && directions[i] != Direction.Center; i++) {
+                if (gc.hasUnitAtLocation(current.add(directions[i]))) {
+                    rocketMoveAway(current.add(directions[i]), cantGo);
+                }
+            }
+            gc.launchRocket(curUnit.id(), marsStart);
+            landSpotNumber++;
+        } else {
+            //System.out.println("Rocket precomputation borked");
+        }
+    }
+
+    public static boolean rocketMoveAway(MapLocation toGo, HashSet<Integer> cantGo) {
+        if (gc.hasUnitAtLocation(toGo)) {
+            Unit unit = gc.senseUnitAtLocation(toGo);
+            if (!gc.isMoveReady(unit.id()) || unit.unitType() == UnitType.Factory || unit.unitType() == UnitType.Rocket) {
+                return false;
+            }
+            for (int i = 0; i < directions.length; i++) {
+                MapLocation temp = toGo.add(directions[i]);
+                if (!cantGo.contains(hash(temp)) && onMap(temp) && Player.gotoable[myId][temp.getX()][temp.getY()]) {
+                    HashSet<Integer> tempCantGo = new HashSet<Integer>();
+                    tempCantGo.addAll(cantGo);
+                    tempCantGo.add(hash(temp));
+                    if (rocketMoveAway(temp, tempCantGo)) {
+                        gc.moveRobot(unit.id(), directions[i]);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean onMap(MapLocation loc) {
+        int x = loc.getX();
+        int y = loc.getY();
+        return x >= 0 && y >= 0 && x < Player.gridX && y < Player.gridY;
+    }
+}
